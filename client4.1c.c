117fa24754c2d892e73b139675a65c8ee241c50c
Ayzmil Amzah <2018276758@isiswa.uitm.edu.my>
2020-11-20 01:25:11 +0800

    client 4.1c

--- client4.1c.c
+++ client4.1c.c
@@ -16,7 +16,7 @@ int main(int argc , char *argv[])
 		printf("Could not create socket");
 	}
 		
-	server.sin_addr.s_addr = inet_addr("192.168.114.6"); //Please enter the ip address of your Server VM
+	server.sin_addr.s_addr = inet_addr("192.168.0.192"); //Please enter the ip address of your Server VM
 	server.sin_family = AF_INET;
 	server.sin_port = htons( 22 );
 
